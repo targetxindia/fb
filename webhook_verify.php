203c8788c725b43a013989722492d634415c48a5
TargetX <76830540+targetxindia@users.noreply.github.com>
2021-08-12 11:52:43 +0530

    Update webhook_verify.php

--- webhook_verify.php
+++ webhook_verify.php
@@ -1,23 +1,9 @@
 <?php
-try {
-  // Returns a `FacebookFacebookResponse` object
-  $response = $fb->post(
-    '/965902793468501/subscriptions',
-    array (
-      'object' => 'page',
-      'fields' => 'leadgen',
-      'verify_token' => 'abc123',
-      'callback_url' => 'https://tx-webhook.herokuapp.com/webhook.php'
-    ),
-    '{access-token}'
-  );
-} catch(FacebookExceptionsFacebookResponseException $e) {
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(FacebookExceptionsFacebookSDKException $e) {
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-$graphNode = $response->getGraphNode();
+  $token = "abc123";
+  $challenge = $_REQUEST['hub.challenge'];
+  $verify_token = $_REQUEST['hub.verify_token'];
 
+  if ($verify_token === $token) {
+    echo $challenge;
+  }
 ?>
