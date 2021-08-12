334292ef92757e83ba66b0159dbe37291629e910
TargetX <76830540+targetxindia@users.noreply.github.com>
2021-08-12 11:39:07 +0530

    Create webhook_verify.php

---
+++ webhook_verify.php
@@ -0,0 +1,20 @@
+try {
+  // Returns a `FacebookFacebookResponse` object
+  $response = $fb->post(
+    '/965902793468501/subscriptions',
+    array (
+      'object' => 'page',
+      'fields' => 'leadgen',
+      'verify_token' => 'abc123',
+      'callback_url' => 'https://tx-webhook.herokuapp.com/webhook.php'
+    ),
+    '{access-token}'
+  );
+} catch(FacebookExceptionsFacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(FacebookExceptionsFacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+$graphNode = $response->getGraphNode();
